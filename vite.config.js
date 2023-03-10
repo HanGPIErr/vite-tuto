3c5d5c51e627f90c94fd1394807c1785bb125554
HanGPIErr <hangpierr@hotmail.fr>
2023-03-10 11:22:56 +0100

    sdfsd

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: 'es2018',
     lib: {
-      entry: resolve(__dirname, 'src', 'my-element.ts'),
-      name: 'my-element',
-      fileName: (format) => `my-element.${format}.js`,
+      entry: 'src/my-element.js',
+      formats: ['es'],
     },
     rollupOptions: {
       external: /^lit/,
